358fd0e15e594ae0901a0bca15d6c96a1e423cc8
Ayush Shrestha <ayush04061@gmail.com>
2025-02-05 13:55:51 +0545

    New file

---
+++ 1.js
@@ -0,0 +1,11 @@
+function sum (num1,num2){
+    console.log(num1+num2)
+}
+
+function sub (num1,num2){
+    console.log(num1-num2)
+}
+
+exports.jod = sum
+exports.bijod = sub
+
---
+++ index.js
@@ -0,0 +1,7 @@
+// // import sum from "./1"
+// const add = require("./1")
+
+// add.jod(5,10)
+// add.bijod(8,2)
+
+(() => console.log(1+2))()
