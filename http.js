075434ecbcbeb7a85ccb0d602f1f7af8b4dd2e90
Ayush Shrestha <ayush04061@gmail.com>
2025-02-07 13:55:44 +0545

    http request and response

---
+++ http.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+
+
+const server = http.createServer((req,res)=> {
+    console.log(req.url,req.method)
+    const path = req.url
+    if (path == "/"){
+        fs.readFile("home.html", (err,data) =>{
+            if(err) res.write(err.message)
+            else{
+                console.log(data)
+                res.end(data)
+            } 
+        })
+    }
+
+    else if (path == "/profile"){
+        fs.readFile("profile.html", (err,data) =>{
+            if(err) res.write(err.message)
+            else{
+                console.log(data)
+                res.end(data)
+             } 
+        })
+    }
+
+    else{
+        res.end("<h1>Not Found</h1>")
+    }
+
+
+})
+
+
+
+server.listen(3000, () => console.log("Server is up and running"))
