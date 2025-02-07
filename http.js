9559dcdf101f53861240320f3801495bcd9831c9
Ayush Shrestha <ayush04061@gmail.com>
2025-02-07 14:09:31 +0545

    Modified the code

--- http.js
+++ http.js
@@ -1,27 +1,24 @@
 const http = require('http');
+
 const fs = require('fs');
 
-
 const server = http.createServer((req,res)=> {
+
     console.log(req.url,req.method)
+
     const path = req.url
+
     if (path == "/"){
         fs.readFile("home.html", (err,data) =>{
             if(err) res.write(err.message)
-            else{
-                console.log(data)
-                res.end(data)
-            } 
+            else res.end(data.toString())
         })
     }
 
     else if (path == "/profile"){
         fs.readFile("profile.html", (err,data) =>{
             if(err) res.write(err.message)
-            else{
-                console.log(data)
-                res.end(data)
-             } 
+            else res.end(data.toString())
         })
     }
 
